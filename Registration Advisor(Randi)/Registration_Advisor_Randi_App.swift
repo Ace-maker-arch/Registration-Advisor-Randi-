769635c2d496efe5248d70125f95f641a51382d8
Odivis Cepeda Jimenez <odiviscepedao2@outlook.com>
2026-04-01 20:24:12 -0400

    Initial Commit

---
+++ Registration Advisor(Randi)/Registration_Advisor_Randi_App.swift
@@ -0,0 +1,17 @@
+//
+//  Registration_Advisor_Randi_App.swift
+//  Registration Advisor(Randi)
+//
+//  Created by Odivis Cepeda Jimenez on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Registration_Advisor_Randi_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
